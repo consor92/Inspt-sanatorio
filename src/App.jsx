b17fb05081c783b0837c6de2a8007279ade8a8ec
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2023-10-02 20:56:48 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Layout from 'modules/Layout'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
